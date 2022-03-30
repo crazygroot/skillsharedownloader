b1b5e20374580b9dfa03be32762ed7a01b9278df
crazygroot <arbindsingh@Arbinds-MacBook-Air.local>
2022-03-30 14:32:17 +0530

    Updated script for Skillsharedownloader

--- ss.py
+++ ss.py
@@ -1,13 +1,20 @@
-import sys, os
+import sys
+import re
 from downloader import Downloader
+
 phpsessid= sys.argv[1]
 cookie = "PHPSESSID="+phpsessid
-
 dl = Downloader(cookie=cookie)
 
 # download by class URL:
 course_url = sys.argv[2]
 dl.download_course_by_url(course_url)
 
-# or by class ID:
-# dl.download_course_by_class_id(189505397)
+if len(sys.argv) != 3:
+    raise Exception('Invalid arguments. Usage : {program} <cookie> <url_or_class_id>'.format(program=sys.argv[0]))
+
+if re.match(r'^[0-9]+$', sys.argv[2]):
+	dl.download_course_by_class_id(sys.argv[2])
+
+else:
+	dl.download_course_by_url(sys.argv[2])
